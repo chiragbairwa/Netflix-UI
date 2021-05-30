7d095259b3add4b87c6407e1f703ffd0fc81330a
Chirag Bairwa <chiragbairwa2k18@gmail.com>
2021-05-30 22:28:17 +0530

    Content Section UI complete

--- src/comp/main.js
+++ src/comp/main.js
@@ -1,10 +1,12 @@
 import './main.css';
+import PartiesCard from './main_parties_corousal';
+import WatchingCard from './main_watching_corousal';
+
 import witcher from "../img/witcher.jpg";
 export default function main() {
    return (
       <div className="main">
          <header>
-
             {/* left-right arrow */}
             <div className="lr_arrow">
                {/* left */}
@@ -29,7 +31,7 @@ export default function main() {
                      <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
                   </svg>
                </i>
-               <input type="text" placeholder="Search..."></input>
+               <input type="text" placeholder="Search..." style={{backgroundColor:'inherit'}}></input>
                <i className="filter_icon">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                      <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z" />
@@ -106,12 +108,30 @@ export default function main() {
          </div>
 
          {/* Parties */}
-         <div>
+         <div className="parties_view">
+            <div>
+               <h2>Parties</h2>
+            </div>
 
+            <div className="parties_crousals">
+               <PartiesCard title="Cadaver" subtitle="Horror marathon"/>
+               <PartiesCard title="Bladerunner" subtitle="sci fi binge"/>
+               <PartiesCard title="Bladerunner" subtitle="sci fi binge"/>
+               <PartiesCard title="Bladerunner" subtitle="sci fi binge"/>
+            </div>
          </div>
 
          {/* Continue watching */}
-         <div></div>
+         <div className="watching_view">
+            <h2>Continue watching</h2>
+
+            <div className="watching_corousal">
+               <WatchingCard title="Cadaver" subtitle="Horror marathon"/>
+               <WatchingCard title="Bladerunner" subtitle="sci fi binge"/>
+               <WatchingCard title="Bladerunner" subtitle="sci fi binge"/>
+               <WatchingCard title="Bladerunner" subtitle="sci fi binge"/>
+            </div>
+         </div>
       </div>
    );
 }
---
+++ src/comp/main_watching_corousal.js
@@ -0,0 +1,33 @@
+export default function cards(props) {
+    return (
+        <div className="parties_crousal">
+            <div className="parties_crousal_img"></div>
+            <div style={{ margin: "-3rem 0 0 5rem", }}>
+                <i className="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                        <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                    </svg>
+                </i>
+                <i className="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                        <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                    </svg>
+                </i>
+                <i className="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                        <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
+                    </svg>
+                </i>
+            </div>
+            <h6 style={{
+                fontSize: "12pt",
+                marginBottom: "-10px"
+            }}>{props.title}</h6>
+            <p>{props.subtitle}</p>
+        </div>
+    )
+
+}
